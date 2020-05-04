657c0848816dc5a262011e5c12826588fced76ee
Mani <manigonzalez82@gmail.com>
2020-05-04 19:59:20 +0800

    Removed added hash to URL on page scroll

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -11,13 +11,10 @@ $(document).ready(function () {
             var hash = this.hash;
 
             // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            // The optional number (1600) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
             }, 1600, function () {
-
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
             });
         } // End if
     });
